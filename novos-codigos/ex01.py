277986c7701662b4235a5fd13e034bcab74255c7
Fernando Conceição <94488257+fernandoxconceicao@users.noreply.github.com>
2024-04-19 18:36:59 +0000

    Novas sugestões

--- novos-codigos/ex01.py
+++ novos-codigos/ex01.py
@@ -1,1 +1,7 @@
 # Neste algoritmo, crie uma variável que armazene uma string e uma lista que armazena várias strings.
+
+minha_string = "Olá, mundo!"
+minha_lista = ["Esta", "é", "uma", "lista", "de", "string"]
+
+print("Minha String: ", minha_string)
+print("Minha Lista: ", minha_lista)
---
+++ novos-codigos/ex02.py
@@ -0,0 +1,2 @@
+# Neste algoritmo, crie uma programa que pergunta o seu nome, sobrenome e idade e cancatena tudo criando uma breve apresentação.
+
